fec77fd5dc44804b76a3f96caab048383b5c1b7f
Laszlo Balogh <laszlo@thebaloghs.com>
2020-09-12 15:53:05 -0600

    Remove boilerplate content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Pokemons from './components/Pokemons'
 
 function App() {
   return (
     <div className="App">
-      <h1>React Pokédex Challenge</h1>
-      <Pokemons />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
