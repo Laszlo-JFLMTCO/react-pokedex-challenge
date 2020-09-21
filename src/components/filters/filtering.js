711a97a1267ff222102bb2da78a6e7680d45eafa
Laszlo Balogh <laszlo@thebaloghs.com>
2020-09-20 23:06:54 -0600

    Remove unused file after Name Filter refactor

--- src/components/filters/filtering.js
+++ src/components/filters/filtering.js
@@ -1,14 +1,14 @@
-function FilterList(inputList, nameFilter) {
-  var filteredList = inputList;
-  if ((inputList.length > 0) && (nameFilter !== "")) {
-    filteredList = [];
-    inputList.forEach(pokemon => {
-      if (pokemon.name.toLowerCase().indexOf(nameFilter.toLowerCase()) > -1) {
-        filteredList.push(pokemon);
-      }
-    });
-  }
-  return filteredList;
-}
+// function FilterList(inputList, nameFilter) {
+//   var filteredList = inputList;
+//   if ((inputList.length > 0) && (nameFilter !== "")) {
+//     filteredList = [];
+//     inputList.forEach(pokemon => {
+//       if (pokemon.name.toLowerCase().indexOf(nameFilter.toLowerCase()) > -1) {
+//         filteredList.push(pokemon);
+//       }
+//     });
+//   }
+//   return filteredList;
+// }
 
-export default FilterList;
+// export default FilterList;
