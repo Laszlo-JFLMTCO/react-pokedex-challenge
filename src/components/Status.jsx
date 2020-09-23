4d9472c9c51093dcf3b704e85d4da0387f06c028
Laszlo Balogh <laszlo@thebaloghs.com>
2020-09-23 09:43:39 -0600

    Replace simple css with Bootstrap styling

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './Status.css';
 
 function mapStateToProps(state) {
   return { 
@@ -12,26 +11,32 @@ function mapStateToProps(state) {
 class Status extends React.Component {
   render() {
     var fetchStatus = false.toString();
-    var fetchStatusStyle = "fetchStatusNOK";
+    var fetchStatusStyle = "alert-danger";
     if (this.props.isLoaded) {
       fetchStatus = this.props.isLoaded.toString()
-      fetchStatusStyle = "fetchStatusOK";
+      fetchStatusStyle = "alert-success";
     }
 
     var fetchError = "none";
-    var fetchErrorStyle = "fetchErrorOK";
+    var fetchErrorStyle = "alert-success";
     if (this.props.error != null) {
       fetchError = this.props.error.toString()
-      fetchErrorStyle = "fetchErrorNOK";
+      fetchErrorStyle = "alert-danger";
     }
 
     return(
       <React.Fragment>
-        <div className={ fetchStatusStyle }>
-          Was JSON from URL loaded: { fetchStatus }
+        <div className={`alert text-center ${fetchStatusStyle}`} role="alert">
+          Was JSON from URL loaded: <strong>{ fetchStatus }</strong>
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-        <div className={ fetchErrorStyle }>
-          Any error? { fetchError }
+        <div className={`alert text-center ${fetchErrorStyle}`}>
+          Any error? <strong>{ fetchError }</strong>
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
       </React.Fragment>
     );
